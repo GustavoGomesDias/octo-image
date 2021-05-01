d3c84b357584ad69f3db50e9ed62d2ec5b2d3f49
GustavoGomesDias <treeelevengg@gmail.com>
2021-05-01 20:21:15 -0300

    Aula 380 - Conexão com o Mongo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/octopics", {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+}).then(() => {
+   console.log("Conected.");
+}).catch(err => console.log(err));
 
 app.get('/', (req, res) => {
    res.json({});
